152b17ccbce521f1c5622a0d5cfe5fe3f486c7b7
Rafi Ahmmed Siyam <rafiahmmed920@gmail.com>
2026-05-07 00:05:05 +0600

    Solved all Problems

--- solutions.ts
+++ solutions.ts
@@ -1,5 +1,3 @@
-// * Solution-1
-
 const filterEvenNumbers = (numOfArray: number[]): number[] => {
    const arrOfEvenNum = [];
 
@@ -12,8 +10,6 @@ const filterEvenNumbers = (numOfArray: number[]): number[] => {
    return arrOfEvenNum;
 };
 
-// * Solution-2
-
 const reverseString = (word: string): string => {
    let revStringArray = [];
    let revLetter = '';
@@ -24,16 +20,58 @@ const reverseString = (word: string): string => {
    return revLetter;
 };
 
-// * Solution-3
-
 type StringOrNumber = string | number;
 const checkType = (value: StringOrNumber): string => {
    if (typeof value === 'string') {
       return 'String';
-   } else if (typeof value === 'number') {
+   } else {
       return 'Number';
    }
 };
 
-const res = checkType("hhsd");
-console.log(res);
+const getProperty = <T, K extends keyof T>(obj: T, key: K) => {
+   return obj[key];
+};
+
+interface Book {
+   title: string;
+   author: string;
+   publishedYear: number;
+}
+
+const toggleReadStatus = (bookObj: Book) => {
+   const modifiedBookObj = {
+      ...bookObj,
+      isRead: true,
+   };
+   return modifiedBookObj;
+};
+
+class Person {
+   name: string;
+   age: number;
+   constructor(name: string, age: number) {
+      this.name = name,
+      this.age = age;
+   }
+}
+
+class Student extends Person {
+   grade: string;
+
+   constructor(name: string, age: number, grade: string) {
+      super(name, age);
+      this.grade = grade;
+   }
+
+   getDetails() {
+      return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+   }
+}
+
+const getIntersection = (arr1: number[], arr2: number[]): number[] => {
+   const result = arr1.filter((num) => arr2.includes(num));
+
+   return result;
+};
+
