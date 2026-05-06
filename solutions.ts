496c9cc2642fd588bd68d2d64e08c9b8a1d03ec3
Rafi Ahmmed Siyam <rafiahmmed920@gmail.com>
2026-05-06 22:26:05 +0600

    Solve 3 Problems

--- solutions.ts
+++ solutions.ts
@@ -1,1 +1,39 @@
-console.log("This is a ts assignment og lavel 2 this is my first ass in l2")
+// * Solution-1
+
+const filterEvenNumbers = (numOfArray: number[]): number[] => {
+   const arrOfEvenNum = [];
+
+   for (let num of numOfArray) {
+      if (num % 2 === 0) {
+         arrOfEvenNum.push(num);
+      }
+   }
+
+   return arrOfEvenNum;
+};
+
+// * Solution-2
+
+const reverseString = (word: string): string => {
+   let revStringArray = [];
+   let revLetter = '';
+   for (let letter of word) {
+      revStringArray.unshift(letter);
+   }
+   revLetter = revStringArray.join('');
+   return revLetter;
+};
+
+// * Solution-3
+
+type StringOrNumber = string | number;
+const checkType = (value: StringOrNumber): string => {
+   if (typeof value === 'string') {
+      return 'String';
+   } else if (typeof value === 'number') {
+      return 'Number';
+   }
+};
+
+const res = checkType("hhsd");
+console.log(res);
